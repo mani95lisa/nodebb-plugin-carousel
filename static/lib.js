2edefe2b488e2e4fd330364c7858bcd3ea128845
mani <mani95lisa@gmail.com>
2014-06-29 20:59:07 +0800

    fix bugs

--- static/lib.js
+++ static/lib.js
@@ -2,15 +2,18 @@
  * Created by mani on 14-6-29.
  */
 $(document).ready(function() {
-    var carousel = $(".carousel");
-    var config = '';
-    try{
-        config = carousel.attr('config');
-        config = jQuery.parseJSON(config);
-    }catch(e){
-        console.log(e);
-    }
-    if(config){
-        carousel.owlCarousel(config);
-    }
+    $(window).on('action:topic.loaded', function () {
+        console.log('loaded');
+        var carousel = $(".carousel");
+        var config = '';
+        try{
+            config = carousel.attr('config');
+            config = jQuery.parseJSON(config);
+        }catch(e){
+            console.log(e);
+        }
+        if(config){
+            carousel.owlCarousel(config);
+        }
+    });
 });
