b190134d7671f641db5a182f75647bab7b7038ee
mani <mani95lisa@gmail.com>
2014-07-01 11:04:59 +0800

    fix bug when enable carousel without images

--- index.js
+++ index.js
@@ -4,17 +4,19 @@
     Carousel.parse = function(postContent, callback) {
         var re = /\[carousel(.*)]/mg;
         var arr = postContent.match(re);
-        if(arr && arr.length != 0){
+        if(arr && arr.length){
             var config = arr[0].replace(re, '$1');
-            postContent = postContent.replace(re, '');
             re = /(<br>|)(<img )src(=".*?>)(<br>|)/mg;
             postContent = postContent.replace(re, '<div class="carousel-img">$2class="lazyOwl" data-src$3</div>');
             re = /<div class="carousel-img.*?<\/div>/mg;
             arr = postContent.match(re);
-            var firstItem = arr[0];
-            postContent = postContent.replace(firstItem, '<div class="carousel" config="'+config+'">'+firstItem);
-            re = /(<div class="carousel.*<\/div>)(.*[^<\/div>])/mg;
-            postContent = postContent.replace(re, '$1</div>$2')
+            if(arr && arr.length){
+                postContent = postContent.replace(/\[carousel(.*)]/mg, '');
+                var firstItem = arr[0];
+                postContent = postContent.replace(firstItem, '<div class="carousel" config="'+config+'">'+firstItem);
+                re = /(<div class="carousel.*<\/div>)(.*[^<\/div>])/mg;
+                postContent = postContent.replace(re, '$1</div>$2')
+            }
         }
         callback(null, postContent);
     };
