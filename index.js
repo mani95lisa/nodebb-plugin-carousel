d2da9d2e9e5bdc93874c7a4b437aa0511b331ee2
Mani Wang <mani95lisa@gmail.com>
2015-07-28 15:30:22 +0800

    upgrade for 0.7.x

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 (function(module) {
     var Carousel = {};
 
-    Carousel.parse = function(postContent, callback) {
+    Carousel.parse = function(data, callback) {
+		if (!data || !data.postData || !data.postData.content) {
+			return callback(null, data);
+		}
+
         var re = /\[carousel(.*)]/mg;
+		var postContent = data.postData.content;
+		var originContent = data.postData.content;
         var arr = postContent.match(re);
         if(arr && arr.length){
             var config = arr[0].replace(re, '$1');
             re = /(<br>|)(<img )src(=".*?>)(<br>|)/mg;
             postContent = postContent.replace(re, '<div class="carousel-img">$2class="lazyOwl" data-src$3</div>');
-            re = /<div class="carousel-img.*?<\/div>/mg;
-            arr = postContent.match(re);
-            if(arr && arr.length){
-                postContent = postContent.replace(/\[carousel(.*)]/mg, '');
-                var firstItem = arr[0];
-                postContent = postContent.replace(firstItem, '<div class="carousel" config="'+config+'">'+firstItem);
-                re = /(<div class="carousel.*<\/div>)(.*[^<\/div>])/mg;
-                postContent = postContent.replace(re, '$1</div>$2')
-            }
+			postContent = postContent.replace(/class="img-responsive img-markdown"/g, '');
+			re = /<div class="carousel-img">.*?\/div>/mg;
+			arr = postContent.match(re);
+			postContent = postContent.replace(re, '');
+			postContent = postContent.replace(/\[carousel(.*)]/mg, '<div class="carousel" config="' + config + '">'+arr.join('')+'</div>');
+			data.postData.content = postContent;
         }
-        callback(null, postContent);
+
+        callback(null, data);
     };
 
     module.exports = Carousel;
